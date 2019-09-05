fee740d701504ac748a5c0e6f6748525b6abb124
zqzal <936952020@qq.com>
2019-09-05 11:42:33 +0800

    2.1开启多进程模式

--- app/src/main/java/com/xuxi/ipcchapter/MainActivity.java
+++ app/src/main/java/com/xuxi/ipcchapter/MainActivity.java
@@ -2,13 +2,37 @@ package com.xuxi.ipcchapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                Intent intent = new Intent(this,SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                Intent intent1 = new Intent(this,ThirdActivity.class);
+                startActivity(intent1);
+                break;
+                default:
+                    break;
+        }
     }
 }
